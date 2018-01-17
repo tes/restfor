e2638ad07264b2440e7ba2d08a209c074440f788
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-01-17 12:36:45 +0100

    init redux

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Provider } from 'react-redux';
 import App from './components/App';
+
+import store from './store';
 
 ReactDOM.render(
   <MuiThemeProvider>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </MuiThemeProvider>,
   document.getElementById('root')
 );
