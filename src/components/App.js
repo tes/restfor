01b7a682b0d4f54d63fcd37c0f4987c951bc1666
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-01-17 12:33:07 +0100

    first steps with react app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 import './App.css';
 
 class App extends Component {
+  state = {
+    isDrawerOpen: false
+  };
+
+  handleToggleDrawer = () => {
+    this.setState({ isDrawerOpen: !this.state.isDrawerOpen });
+  };
+
   render() {
     return (
       <div className="App">
+        <AppBar
+          title="Restfor"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+          onLeftIconButtonClick={this.handleToggleDrawer}
+        />
+        <Drawer open={this.state.isDrawerOpen} docked={false} onRequestChange={this.handleToggleDrawer}>
+          <MenuItem onClick={this.handleToggleDrawer}>Menu Item</MenuItem>
+          <MenuItem onClick={this.handleToggleDrawer}>Menu Item 2</MenuItem>
+        </Drawer>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById('root')
+);
