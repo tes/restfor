7edebdc30cc495cea1a06fddd22f84167acf6898
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-01-17 13:47:41 +0100

    use tabs for listing entities

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,31 @@ import { connect } from 'react-redux';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
+import { Tabs, Tab } from 'material-ui/Tabs';
 import './App.css';
 import { fetchEntities } from '../actionCreators';
 
 class App extends Component {
   state = {
-    isDrawerOpen: false
+    isDrawerOpen: false,
+    tab: null
   };
 
   componentDidMount() {
     this.props.fetchEntities();
   }
 
+  componentWillUpdate(nextProps) {
+    if (this.props.entities !== nextProps.entities && nextProps.entities.length > 0) {
+      this.setState({ tab: nextProps.entities[0].toLowerCase() });
+    }
+  }
+
   handleToggleDrawer = () => {
     this.setState({ isDrawerOpen: !this.state.isDrawerOpen });
   };
+
+  handleTabChange = value => this.setState({ tab: value });
 
   render() {
     const { entities } = this.props;
@@ -29,12 +39,22 @@ class App extends Component {
           onLeftIconButtonClick={this.handleToggleDrawer}
         />
         <Drawer open={this.state.isDrawerOpen} docked={false} onRequestChange={this.handleToggleDrawer}>
-          {entities.map(entity => (
-            <MenuItem key={entity} onClick={this.handleToggleDrawer}>
-              {entity}
-            </MenuItem>
-          ))}
+          <MenuItem onClick={this.handleToggleDrawer}>Menu Item</MenuItem>
+          <MenuItem onClick={this.handleToggleDrawer}>Menu Item 2</MenuItem>
         </Drawer>
+        <Tabs value={this.state.tab} onChange={this.handleTabChange}>
+          {entities.map(entity => (
+            <Tab label={entity} value={entity.toLowerCase()}>
+              <div>
+                <h2>{entity}</h2>
+                <p>
+                  Tabs are also controllable if you want to programmatically pass them their values. This allows for
+                  more functionality in Tabs such as not having any Tab selected or assigning them different values.
+                </p>
+              </div>
+            </Tab>
+          ))}
+        </Tabs>
       </div>
     );
   }
