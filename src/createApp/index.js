b5a710f6278e56411c8499ffaf41425117b155df
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-01-25 10:10:20 +0100

    wip

--- src/createApp/index.js
+++ src/createApp/index.js
@@ -4,18 +4,11 @@ import { Provider as StoreProvider } from 'react-redux';
 import { Router, Route } from 'react-router-dom';
 import ViewProvider from './components/ViewProvider';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
-import blue from 'material-ui/colors/blue';
 import App from './components/App';
 import createApi from './createApi';
 import createHashHistory from 'history/createHashHistory';
 import createStore from './createStore';
 import createViews from './createViews';
-
-const theme = createMuiTheme({
-  palette: {
-    primary: blue
-  }
-});
 
 export default (config, viewFactory = () => {}) => {
   const theme = createMuiTheme();
@@ -24,7 +17,7 @@ export default (config, viewFactory = () => {}) => {
   const store = createStore({ api, hashHistory });
   const views = createViews(viewFactory);
   return (
-    <MuiThemeProvider >
+    <MuiThemeProvider theme={theme}>
       <ViewProvider views={views}>
         <StoreProvider store={store}>
           <Router history={hashHistory}>
