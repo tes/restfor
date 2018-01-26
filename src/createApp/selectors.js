3cc3971ba380f1eb1a8f74c12f3de16b626a1407
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-01-26 09:54:58 +0100

    add selectors and use them in App

--- src/createApp/selectors.js
+++ src/createApp/selectors.js
@@ -1,5 +1,23 @@
+import { resolvePage } from './helpers/page';
+
+export const getResourceName = ({ router: { params: { resourceName } } }) => resourceName;
+
+export const getId = ({ router: { params: { id } } }) => (id ? Number(id) : null);
+
+export const getPage = ({ router: { query: { page } } }) => resolvePage(page);
+
 export const getMaxPage = resourceName => ({ settings: { limit }, resources }) => {
   const resource = resources[resourceName];
   if (!resource) return null;
   return Math.ceil(resource.count / limit);
 };
+
+export const getLimit = ({ settings: { limit } }) => limit;
+
+export const getItems = state => {
+  const resourceName = getResourceName(state);
+  const { resources } = state;
+  return resources[resourceName] && resources[resourceName].items;
+};
+
+export const getSchemaList = ({ schemas }) => Object.keys(schemas);
