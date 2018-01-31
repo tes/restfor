65426b91fe84c4c63957e7f36fe505dfacc96d0e
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-01-31 12:37:27 +0100

    wip graphql

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-const { makeExecutableSchema } = require('graphql-tools');
-const getSchemas = require('./getSchemas');
-const getResolvers = require('./getResolvers');
+const { makeExecutableSchema, mergeSchemas } = require('graphql-tools');
+const { graphql, buildASTSchema } = require('graphql');
+const { parse } = require('graphql/language');
+const { introspectionQuery } = require('graphql/utilities');
+const { readFileSync, writeFileSync } = require('fs');
 
-module.exports = ({ db: dbConfig, schemasPath, resolversPath }) => {
-  const typeDefs = getSchemas(schemasPath);
-  const resolvers = getResolvers(resolversPath);
-  const schema = makeExecutableSchema({ typeDefs, resolvers });
+module.exports = ({ db: dbConfig, schemas, resolvers }) => {
+  console.log(JSON.stringify(parse(`
+  type User {
+    id: Int @primaryKey @auto
+    name: String
+  }
+  
+  type Task {
+    id: Int @primaryKey @auto
+    title: String @required
+    taskType: TaskType
+    checked: Boolean @default(value: false)
+    userId: Int @required
+  }
+  
+  enum TaskType {
+    PRIMARY
+    SECONDARY
+  }
+  
+  `), null, 2))
+  const schemaFile = readFileSync(schemas);
+  const schemaDocument = parse(schemaFile);
+  //const schema = mergeSchemas({ schemas, resolvers });
+  //const schema = makeExecutableSchema({ typeDefs: schemas, resolvers });
   const router = express.Router();
-  router.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-  router.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+  //router.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+  //router.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
   return router;
 };
