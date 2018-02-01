edf1339e425aeb4b1a18118c3376eb407a27bcbc
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-02-01 16:52:50 +0100

    fix router bug

--- src/app/reducers/router.js
+++ src/app/reducers/router.js
@@ -1,23 +1,22 @@
 import { LOCATION_CHANGE } from 'react-router-redux';
 import { matchRoutes } from 'react-router-config';
 import queryString from 'query-string';
-import { matchPath,  } from 'react-router';
+import { matchPath } from 'react-router';
 const matchOptions = { path: '/:resourceName?/:id?', exact: false };
-const routeOptions = [ 
+const routeOptions = [
   { path: '/:resourceName', exact: true },
   { path: '/:resourceName/item/:id', exact: true },
   { path: '/:resourceName/segment/:segment/', exact: true },
   { path: '/:resourceName/segment/:segment/item/:id', exact: true },
   { path: '/:resourceName/item/:id/edit', exact: true },
-  { path: '/:resourceName/segment/:segment/item/:id/edit', exact: true },
-]
+  { path: '/:resourceName/segment/:segment/item/:id/edit', exact: true }
+];
 
 export default (state = {}, action) => {
   switch (action.type) {
     case LOCATION_CHANGE: {
-      const [ firstMatch ] = matchRoutes(routeOptions, action.payload.pathname)
-      if (!firstMatch) return state;
-      const { match, route } = firstMatch;
+      const [firstMatch] = matchRoutes(routeOptions, action.payload.pathname);
+      const { match } = firstMatch || {};
       return {
         ...action.payload,
         query: queryString.parse(action.payload.search),
